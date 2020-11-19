f95c2094c0f2268612e005aeff3f7df48e51dcad
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-19 17:15:06 +0700

    PBOIF2-10119082-Latihan60-Akatsuki

---
+++ src/pboif2/pkg10119082/latihan60/akatsuki/PBOIF210119082Latihan60Akatsuki.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan60.akatsuki;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Akatsuki
+ */
+public class PBOIF210119082Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Anggota a = new Anggota();
+        a.tampilProfil();
+    } 
+}
